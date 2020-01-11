7d119f2d8623eb623a7be2bd5f6b79023fd033b8
dragon20180618 <715077252@qq.com>
2020-01-11 20:20:00 +0800

    update

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import pygame as p
 import time
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 def run_game():
     #初始化游戏，并创建屏幕
     p.init()
-    ai_game = Settings()
-    screen = p.display.set_mode((ai_game.screen_height,ai_game.screen_width))
+    ai_settings = Settings()
+    screen = p.display.set_mode((ai_settings.screen_height,ai_settings.screen_width))
     p.display.set_caption("Alien Invalid")
     ship = Ship(screen)
     while True:
-        for event in p.event.get():
-            if event.type == p.QUIT:
-                sys.exit()
-        screen.fill(ai_game.bg_color)
+        gf.check_events()
+        screen.fill(ai_settings.bg_color)
         ship.blitme()
         p.display.flip()
 run_game()
+    
+`
