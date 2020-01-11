6169764c544d45aa9c0a051da0b441ef2a768411
dragon20180618 <715077252@qq.com>
2020-01-11 20:06:24 +0800

    update

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import sys
 import pygame as p
 import time
 from settings import Settings
+from ship import Ship
 def run_game():
     #初始化游戏，并创建屏幕
     p.init()
     ai_game = Settings()
     screen = p.display.set_mode((ai_game.screen_height,ai_game.screen_width))
     p.display.set_caption("Alien Invalid")
-    print(ai_game.bg_color)
+    ship = Ship(screen)
     while True:
         for event in p.event.get():
             if event.type == p.QUIT:
                 sys.exit()
         screen.fill(ai_game.bg_color)
+        ship.blitme()
         p.display.flip()
 run_game()
--- ship.py
+++ ship.py
@@ -2,7 +2,7 @@ import pygame
 class Ship():
     def __init__(self,screen):
         self.screen=screen
-        self.image = pygame.image.load("image\\plane.bmp")
+        self.image = pygame.image.load("images/plane.bmp")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect();
         self.rect.centerx = self.screen_rect.centerx
