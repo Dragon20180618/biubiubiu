c5437c19d94c0588aec0d45e69e0fbe4e4648fec
dragon20180618 <715077252@qq.com>
2020-01-11 19:58:14 +0800

    update

--- main.py
+++ main.py
@@ -5,26 +5,14 @@ from settings import Settings
 def run_game():
     #初始化游戏，并创建屏幕
     p.init()
-    screen=p.display.set_mode((1200,800))
-    p.display.set_caption("Alien Invasion")
-    bg_color=(230,230,230)
-    i=1
-    j=1
-    k=1
+    ai_game = Settings()
+    screen = p.display.set_mode((ai_game.screen_height,ai_game.screen_width))
+    p.display.set_caption("Alien Invalid")
+    print(ai_game.bg_color)
     while True:
-        if i==230:
-            i=1
-            
-        if k==230:
-            k=1
-        
-        bg_color=(230,k,i)
-        i+=1
-        k+=1
         for event in p.event.get():
             if event.type == p.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_game.bg_color)
         p.display.flip()
-    #time.sleep
 run_game()
